b948ad443e8ae63bca937ebe1948c36ec9218b64
jmma2631 <jmma2631@outlook.com>
2026-09-10 20:06:07 +0000

    Configure Azure database connection

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-builder.Services.AddDbContext<MyDatabaseContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-}
+builder.Services.AddDbContext<MyDatabaseContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+    builder.Services.AddStackExchangeRedisCache(options => 
+    { 
+    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+    options. InstanceName = "SampleInstance";
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
