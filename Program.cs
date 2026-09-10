863c036b63af7e363ce8107cbc1ee4ac315e43ab
jmma2631 <jmma2631@outlook.com>
2026-09-10 21:16:44 +0000

    fix

--- Program.cs
+++ Program.cs
@@ -12,12 +12,7 @@ else
 {
 builder.Services.AddDbContext<MyDatabaseContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-    builder.Services.AddStackExchangeRedisCache(options => 
-    { 
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options. InstanceName = "SampleInstance";
-    });
-
+}    
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
